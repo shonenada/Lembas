85c95d08591137a5056f4ba7d6c75caa28cba3e1
shonenada <shonenada@gmail.com>
2017-10-19 13:44:05 +0800

    feat: mime support

--- lembas/master/views.py
+++ lembas/master/views.py
@@ -1,27 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from flask import Blueprint, request, url_for, abort, Response, jsonify
+from flask import Blueprint, request, url_for, abort, Response, jsonify, render_template_string
 
 from lembas.store import current_store
 
 
 bp = Blueprint('master', __name__)
 
+mimes = {
+    'txt': 'plain/text',
+    'json': 'application/json',
+    'svg': 'image/svg+xml',
+}
+
 
 @bp.route('/')
 def index():
     return jsonify(
-        host=url_for('master.index', _external=True)
-    )
+        host=url_for('master.index', _external=True))
 
 
-@bp.route('/<key>')
-def get_key(key):
+@bp.route('/<key>.<ext>')
+def get_key(key, ext):
     value = current_store.get(key)
     if value is None:
         return abort(404)
-    return Response(value, mimetype='application/json')
+    mime = mines.get(ext, 'plain/text')
+    data = request.args.to_dist()
+    tml = render_template_string(value, **data)
+    return Response(value, mimetype=mime)
 
 
 @bp.route('/_/<key>', methods=['POST', 'PUT', 'PATCH'])
@@ -29,8 +37,9 @@ def set_key(key):
     data = request.data
     if data is None:
         return abort(400)
+    ext = request.args.get('ext', 'json')
     current_store.set(key, data)
-    url = url_for('master.get_key', key=key, _external=True)
+    url = url_for('master.get_key', key=key, ext=ext, _external=True)
     return jsonify(success=True, url=url)
 
 
